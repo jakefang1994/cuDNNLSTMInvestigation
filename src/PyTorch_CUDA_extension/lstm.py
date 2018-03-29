78c7aab2cc9b4fcdf43376b3a06f326cd5f0523e
DESKTOP-PV4VR8Q\40737 <shuo.niu@mail.utoronto.ca>
2018-03-28 21:52:14 -0400

    Add torch.nn.LSTM() implementation for comparison

--- src/PyTorch_CUDA_extension/lstm.py
+++ src/PyTorch_CUDA_extension/lstm.py
@@ -7,8 +7,6 @@ class lstm(torch.autograd.Function):
 
     def forward(self, h_data, x_data, c_data, hiddenSize, miniBatch, seqLength, numLayers):
         if h_data.is_cuda == True and x_data.is_cuda == True and c_data.is_cuda == True:
-            time = _ext.lstm.lstm_forward(h_data, x_data, c_data, hiddenSize, miniBatch, seqLength, numLayers)
+            _ext.lstm.lstm_forward(h_data, x_data, c_data, hiddenSize, miniBatch, seqLength, numLayers)
         else:
             raise NotImplementedError()
-        
-        return time
--- src/PyTorch_CUDA_extension/test.py
+++ src/PyTorch_CUDA_extension/test.py
@@ -1,14 +1,9 @@
 import sys
+import time
 import torch
 from lstm import lstm
 
-class Network(torch.nn.Module):
-    def __init__(self):
-        super(Network, self).__init__()
-
-    def forward(self, h_data, x_data, c_data, hiddenSize, miniBatch, seqLength, numLayers):
-        return lstm().forward(h_data, x_data, c_data, hiddenSize, miniBatch, seqLength, numLayers)
-
+# two networks share hyperparameters:
 argv = sys.argv
 if len(argv) == 5:
     hidden_size = int(argv[1])
@@ -24,19 +19,50 @@ else:
     print("Usage: python test.py <hiddenSize> <miniBatch> <seqLength> <numLayers>")
     raise NotImplementedError()
 
-# random inputs
-num_elements = hidden_size * mini_batch
-h_data = torch.randn((seq_length+1)*num_layer*num_elements).cuda()
-x_data = torch.randn(seq_length*(num_layer+1)*num_elements).cuda()
-c_data = torch.randn((seq_length+1)*num_layer*num_elements).cuda()
-
-net = Network().cuda()
-
 hidden_size_tensor = torch.autograd.Variable(torch.IntTensor([hidden_size]))
 mini_batch_tensor = torch.autograd.Variable(torch.IntTensor([mini_batch]))
 seq_length_tensor = torch.autograd.Variable(torch.IntTensor([seq_length]))
 num_layer_tensor = torch.autograd.Variable(torch.IntTensor([num_layer]))
 
-time = net.forward(h_data, x_data, c_data, hidden_size, mini_batch, seq_length, num_layer)
 
-print("Time: ", time)
+class Custom_Network(torch.nn.Module):
+    def __init__(self):
+        super(Custom_Network, self).__init__()
+
+    def forward(self, hiddenSize, miniBatch, seqLength, numLayers):
+        # random inputs
+        num_elements = hidden_size * mini_batch
+        self.x_data = torch.randn(seq_length*(num_layer+1)*num_elements).cuda()
+        self.h_data = torch.randn((seq_length+1)*num_layer*num_elements).cuda()
+        self.c_data = torch.randn((seq_length+1)*num_layer*num_elements).cuda()
+
+        lstm().forward(self.h_data, self.x_data, self.c_data, hiddenSize, miniBatch, seqLength, numLayers)
+        
+class Official_Network(torch.nn.Module):
+    def __init__(self, hiddenSize, miniBatch, seqLength, numLayers):
+        super(Official_Network, self).__init__()
+        self.hiddenSize = hiddenSize
+        self.miniBatch = miniBatch
+        self.seqLength = seqLength
+        self.numLayers = numLayers
+        self.lstm = torch.nn.LSTM(hiddenSize, hiddenSize, numLayers, batch_first=False)
+
+    def forward(self, x):
+        self.h_data = torch.autograd.Variable(torch.randn(self.numLayers, self.miniBatch, self.hiddenSize)).cuda()
+        self.c_data = torch.autograd.Variable(torch.randn(self.numLayers, self.miniBatch, self.hiddenSize)).cuda()
+
+        out, _ = self.lstm(x, (self.h_data, self.c_data))
+
+
+net = Custom_Network().cuda()
+start_time = time.time()
+net.forward(hidden_size, mini_batch, seq_length, num_layer)
+elapsed_time = time.time() - start_time
+print("Custom time:\t%f seconds"%(elapsed_time))
+
+net = Official_Network(hidden_size, mini_batch, seq_length, num_layer).cuda()
+start_time = time.time()
+x_data = torch.autograd.Variable(torch.randn(seq_length, mini_batch, hidden_size)).cuda()
+net.forward(x_data)
+elapsed_time = time.time() - start_time
+print("Official time:\t%f seconds"%(elapsed_time))
