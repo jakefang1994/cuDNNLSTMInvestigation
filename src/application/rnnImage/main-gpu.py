6b1606846c8c2166fbd76862edc6016396cc9039
Guliisgreat <guliisgreat@hotmail.com>
2018-04-05 16:04:56 -0400

    (sequence, batch, hidden)

--- src/application/rnnImage/main-gpu.py
+++ src/application/rnnImage/main-gpu.py
@@ -1,38 +1,39 @@
-import torch 
+import torch
 import torch.nn as nn
 import torchvision.datasets as dsets
 import torchvision.transforms as transforms
 from torch.autograd import Variable
-
+import numpy as np
 
 # Hyper Parameters
 sequence_length = 28
 input_size = 28
-hidden_size = 128
+hidden_size = 28
 num_layers = 2
 num_classes = 10
 batch_size = 100
-num_epochs = 2
+num_epochs = 3
 learning_rate = 0.01
 
 # MNIST Dataset
 train_dataset = dsets.MNIST(root='./data/',
-                            train=True, 
+                            train=True,
                             transform=transforms.ToTensor(),
                             download=True)
 
 test_dataset = dsets.MNIST(root='./data/',
-                           train=False, 
+                           train=False,
                            transform=transforms.ToTensor())
 
 # Data Loader (Input Pipeline)
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                           batch_size=batch_size, 
+                                           batch_size=batch_size,
                                            shuffle=True)
 
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
-                                          batch_size=batch_size, 
+                                          batch_size=batch_size,
                                           shuffle=False)
+
 
 # RNN Model (Many-to-One)
 class RNN(nn.Module):
@@ -40,20 +41,20 @@ class RNN(nn.Module):
         super(RNN, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=False)
         self.fc = nn.Linear(hidden_size, num_classes)
-    
+
     def forward(self, x):
-        # Set initial states 
-        h0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size).cuda()) 
-        c0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size).cuda())
-        
+        # Set initial states
+        h0 = Variable(torch.zeros(self.num_layers, batch_size, self.hidden_size).cuda())
+        c0 = Variable(torch.zeros(self.num_layers, batch_size, self.hidden_size).cuda())
+
         # Forward propagate RNN
-        out, _ = self.lstm(x, (h0, c0))  
-        
+        out, _ = self.lstm(x, (h0, c0))  # out -> (100, 28, 28) (miniBatch, seqLength, hiddenSize)
         # Decode hidden state of last time step
-        out = self.fc(out[:, -1, :])  
+        out = self.fc(out[-1, :, :])
         return out
+
 
 rnn = RNN(input_size, hidden_size, num_layers, num_classes)
 rnn.cuda()
@@ -61,35 +62,42 @@ rnn.cuda()
 # Loss and Optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(rnn.parameters(), lr=learning_rate)
-    
+
 # Train the Model
 for epoch in range(num_epochs):
     for i, (images, labels) in enumerate(train_loader):
-        images = Variable(images.view(-1, sequence_length, input_size)).cuda()  
+        images = images.numpy().reshape([batch_size, sequence_length, input_size]).transpose(1,0,2)
+
+
+        # images = images.view( batch_size, sequence_length, input_size)
+
+        images = Variable(torch.from_numpy(images)).cuda()
         labels = Variable(labels).cuda()
-        
+
         # Forward + Backward + Optimize
         optimizer.zero_grad()
         outputs = rnn(images)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
-        
-        if (i+1) % 100 == 0:
-            print ('Epoch [%d/%d], Step [%d/%d], Loss: %.4f' 
-                   %(epoch+1, num_epochs, i+1, len(train_dataset)//batch_size, loss.data[0]))
+
+        if (i + 1) % 100 == 0:
+            print ('Epoch [%d/%d], Step [%d/%d], Loss: %.4f'
+                   % (epoch + 1, num_epochs, i + 1, len(train_dataset) // batch_size, loss.data[0]))
 
 # Test the Model
 correct = 0
 total = 0
 for images, labels in test_loader:
-    images = Variable(images.view(-1, sequence_length, input_size)).cuda()
+    images = images.numpy().reshape([batch_size, sequence_length, input_size]).transpose(1, 0, 2)
+    images = Variable(torch.from_numpy(images)).cuda()
+
     outputs = rnn(images)
     _, predicted = torch.max(outputs.data, 1)
     total += labels.size(0)
     correct += (predicted.cpu() == labels).sum()
 
-print('Test Accuracy of the model on the 10000 test images: %d %%' % (100 * correct / total)) 
+print('Test Accuracy of the model on the 10000 test images: %d %%' % (100 * correct / total))
 
 # Save the Model
 torch.save(rnn.state_dict(), 'rnn.pkl')
